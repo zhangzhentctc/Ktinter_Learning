d600da21e97cdcff8172442e24104465ad45ac69
Aaron Zhen Zhang <zhangzhentctc@163.com>
2018-01-07 01:50:16 +0800

    Improve Shared Mem

--- shared_mem/client.py
+++ shared_mem/client.py
@@ -4,74 +4,47 @@ import time
 import threading
 import json
 
-FILE_NAME = "./test.dat"
-FILE_SIZE = 1024
+
 
 class client(threading.Thread):
-    def __init__(self, mutex):
+    def __init__(self, mutex, buff):
         super(client, self).__init__()
         self.mutex = mutex
+        self.buff = buff
         self.ready = False
 
-    def open_shared_mem(self):
-        try:
-            self.f_handler = open(FILE_NAME, "r+")
-            self.if_sm_open = True
-            print("[Client] Open Shared Mem OK")
-        except:
-            self.if_sm_open = False
-            print("[Client] Open Shared Mem Fail")
-        return
-
-    def if_ready(self):
-        if self.if_sm_open == True:
-            self.ready = True
+    def fetch_data(self):
+        self.buff.open_mmap()
+        ret, s = self.buff.buffer_read()
+        if s != "":
+            sm_data = json.loads(s)
+            list = sm_data["data"]
+            val = list.pop(0)
+            if len(list) == 0:
+                json_str = ""
+            else:
+                data = {}
+                data["data"] = list
+                json_str = json.dumps(data)
+            #print("[Client] Store " + json_str)
+            self.buff.buffer_reset()
+            self.buff.buffer_write(json_str)
+            self.buff.close_mmap()
         else:
-            self.ready = False
+            val = []
 
-    def prepare_client(self):
-        self.open_shared_mem()
-        self.if_ready()
+        return val
+
 
     def run(self):
-        self.prepare_client()
-        if self.ready == True:
-            print("[Client] Start")
-            while True:
-                if self.mutex.acquire(1):
-                    print("[Client] Get Lock")
-                    try:
-                        m = mmap.mmap(self.f_handler.fileno(), FILE_SIZE, access=mmap.ACCESS_WRITE)
-                    except:
-                        print("[Client] MMap Fail")
-                        self.mutex.release()
-                        time.sleep(0.2)
-                        continue
-                    s = m.read(FILE_SIZE).decode().replace('\x00', '')
-                    if s != "":
-                        sm_data = json.loads(s)
-                        list = sm_data["data"]
-                        val = list.pop(0)
-                        if len(list) == 0:
-                            json_str=""
-                        else:
-                            data = {}
-                            data["data"] = list
-                            json_str = json.dumps(data)
-                        print("[Client] Store "+ json_str)
-
-                        #json_str.rjust(FILE_SIZE, '\x00')
-                        m.seek(0)
-                        blank = "\0" * FILE_SIZE
-                        m.write(blank.encode())
-                        m.seek(0)
-                        m.write(json_str.encode())
-                        m.flush()
-                        m.close()
-                        print("[Client] Get")
-                        print(val)
-                    self.mutex.release()
-                    print("[Client] Release Lock")
-                time.sleep(0.5)
+        while True:
+            if self.mutex.acquire(1):
+                #print("[Client] Get Lock")
+                val = self.fetch_data()
+                #print("[Client] Fetch Data")
+                if val != []:
+                    print(val)
+                self.mutex.release()
+                #print("[Client] Release Lock")
+            time.sleep(0.08)
         print("[Client] End")
-
--- shared_mem/server.py
+++ shared_mem/server.py
@@ -7,120 +7,61 @@ import random
 import json
 
 from shared_mem.client import *
-
-
-FILE_NAME = "./test.dat"
-FILE_SIZE = 1024
-
-
+from shared_mem.store_buffer import *
 
 
 class server(threading.Thread):
-    def __init__(self, mutex):
+    def __init__(self, mutex, buff):
         super(server, self).__init__()
         self.mutex = mutex
+        self.buff = buff
         self.all_ready = False
 
-    def init_all(self):
-        self.f_handler = open(FILE_NAME, "w")
-        self.f_handler.write('\x00' * FILE_SIZE)
-        self.f_handler.close()
-        self.f_handler = open(FILE_NAME, "r+")
-        self.mmap = mmap.mmap(self.f_handler.fileno(), FILE_SIZE, access=mmap.ACCESS_WRITE)
 
-    def create_shared_mem(self):
-        try:
-            self.f_handler = open(FILE_NAME, "w")
-            self.f_handler.write('\x00' * FILE_SIZE)
-            self.f_handler.close()
-            self.if_sm_create = True
-            print("[Server] Create Shared Mem OK")
-        except:
-            self.if_sm_create = False
-            print("[Server] Create Shared Mem Fail")
-
-    def open_shared_mem_file(self):
-        try:
-            self.f_handler = open(FILE_NAME, "r+")
-            self.if_sm_f_open = True
-            print("[Server] Open Shared Mem OK")
-        except:
-            self.if_sm_f_open = False
-            print("[Server] Open Shared Mem Fail")
-        return
-
-    def open_shared_mem(self):
-        try:
-            self.mmap = mmap.mmap(self.f_handler.fileno(), FILE_SIZE, access=mmap.ACCESS_WRITE)
-            self.if_mmap_open = True
-        except:
-            self.if_mmap_open = False
-            print("[Server] Open MMAP Fail")
-
-    def if_ready(self):
-        if self.if_sm_f_open == True and \
-           self.if_sm_create == True:
-            self.all_ready = True
-            print("[Server] All OK")
-        else:
-            self.all_ready = False
-
-    def prepare_client(self):
-        self.create_shared_mem()
-        self.open_shared_mem_file()
-        self.open_shared_mem()
-        self.if_ready()
-
-    def generate_new_data(self):
+    def generate_new_data(self, i):
         a = random.randint(10000, 20000)
         b = random.randint(10000, 20000)
         c = random.randint(10000, 20000)
-        return [a,b,c]
+        return [i, a,b,c]
+
+    def send_data(self, new_data):
+        self.buff.open_mmap()
+        ret, s = self.buff.buffer_read()
+        if s != "":
+            print("[Server] Get sm:" + s)
+            old_data = json.loads(s)
+            list = old_data["data"]
+        else:
+            list = []
+        list.append(new_data)
+        data = {}
+        data["data"] = list
+        json_str = json.dumps(data)
+        self.buff.buffer_reset()
+        self.buff.buffer_write(json_str)
+        self.buff.close_mmap()
 
     def run(self):
-        self.prepare_client()
-        if self.all_ready == True:
-            print("[Server] Start")
-
-            for i in range(1, 10001):
-                if self.mutex.acquire(1):
-                    print("[Server] Get Lock")
-                    self.open_shared_mem()
-                    self.mmap.seek(0)
-                    s = self.mmap.read(FILE_SIZE).decode().replace('\x00', '')
-                    if s != "":
-                        print("[Server] Get sm:" + s)
-                        old_data = json.loads(s)
-                        list = old_data["data"]
-                    else:
-                        list = []
-                    # new_data = self.generate_new_data()
-                    new_data = [i, i, i]
-                    list.append(new_data)
-                    data = {}
-                    data["data"] = list
-                    json_str = json.dumps(data)
-
-                    # json_str.rjust(FILE_SIZE, '\x00')
-                    self.f_handler.write('\x00' * FILE_SIZE)
-                    self.mmap.seek(0)
-                    self.mmap.write(json_str.encode())
-                    self.mmap.flush()
-                    self.mmap.close()
-                    print("[Server] Release Lock")
-                    self.mutex.release()
-                    print("[Server] " + json_str)
-
-                time.sleep(random.random())
+        for i in range(0, 100000):
+            if self.mutex.acquire(1):
+                #print("[Server] Get Lock")
+                new_data = self.generate_new_data(i)
+                self.send_data(new_data)
+                #print("[Server] Send Data")
+                time.sleep(random.random() * 0.8)
+                #print("[Server] Release Lock")
+                self.mutex.release()
         print("[Server] End")
 
 if __name__ == '__main__':
     mutex_lock = threading.Lock()
-    serv = server(mutex_lock)
+    buff = store_buffer()
+    buff.initialize()
+    serv = server(mutex_lock, buff)
     serv.start()
     time.sleep(0.2)
-    if serv.all_ready == True:
+    if serv.all_ready == True or True:
         print("Open Client")
-        client = client(mutex_lock)
+        client = client(mutex_lock, buff)
         client.start()
 
